9012dfaaae0b3bd7b916377674d7f053c4848e4f
Rabiul Khan <91216874+rabiulkhan07@users.noreply.github.com>
2021-09-23 16:22:14 +0600

    finish

---
+++ PalindromeSting.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+
+public class PalindromeString {
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String : ");
+        String originalString = sc.nextLine();
+        
+        String a = reverseNum(originalString);
+        System.out.println(a);
+        
+        //compare
+        if(originalString.equals(a)){
+            System.out.println(originalString + " is a palindrome String.");
+        }
+        else{
+            
+            System.out.println(originalString + " is not a palindrome String.");
+        }
+    }
+    public static String reverseNum(String num){
+        String reverse = "";
+        
+        for(int i = num.length()-1; i >= 0; i--){
+            reverse = reverse + num.charAt(i);
+            
+        }
+        return reverse;
+        
+    }
+    
+    
+}
